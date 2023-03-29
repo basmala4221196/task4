d8d3081212d5a5dab71209eb45bf8cc3739a8464
basmala4221196 <126463264+basmala4221196@users.noreply.github.com>
2023-03-29 22:35:33 +0200

    Add files via upload
    # task4
    
    Self-Join:
    A self-join is a type of join operation where a table is joined with itself. In other words, it is a way to combine rows from the same table based on a common column. Self-joins are useful when you need to compare data within the same table.
    
    Implicit Join:
    An implicit join is a type of join that uses the WHERE clause to specify the join condition. In an implicit join, you do not use the JOIN keyword or specify the type of join. Instead, you simply list the tables in the FROM clause and use the WHERE clause to specify how they should be joined.
    
    Self-Outer Join:
    A self-outer join is similar to a self-join, but it includes all rows from one table and matching rows from another table. In other words, it returns all rows from one table even if there are no matches in the other table.
    
    Natural Join:
    A natural join is a type of join that combines two tables based on columns with matching names and data types. In other words, it automatically matches columns with the same name and data type in both tables.
    
    Cross Join:
    A cross join (also known as a Cartesian product) returns all possible combinations of rows from two tables. It does not require any matching criteria or conditions.
    
    Union:
    A union combines two or more SELECT statements into a single result set. It returns all distinct rows from each SELECT statement and removes duplicates. The number and order of columns must be identical in each SELECT statement being combined.

---
+++ task4.sql
@@ -0,0 +1,96 @@
+CREATE DATABASE markete;
+
+CREATE TABLE Customers (
+  customer_id INT PRIMARY KEY,
+  customer_name VARCHAR(50),
+  email VARCHAR(50)
+);
+
+CREATE TABLE Products (
+  product_id INT PRIMARY KEY,
+  product_name VARCHAR(50),
+  price DECIMAL(10,2)
+);
+
+CREATE TABLE Orders (
+  order_id INT PRIMARY KEY,
+  customer_id INT,
+  product_id INT,
+  quantity INT,
+  FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
+  FOREIGN KEY (product_id) REFERENCES Products(product_id)
+); 
+
+
+
+
+
+INSERT INTO Products (product_id, product_name, price) VALUES
+(1, 'Product A', 15.99),
+(2, 'Product B', 22.99),
+(3, 'Product C', 35.99);
+
+INSERT INTO Customers (customer_id, customer_name, email) VALUES
+(1, 'mark', 'dfiefh@email.com'),
+(2, ' jack', 'fgaih@email.com'),
+(3, 'Jony', 'bofhirohnson@email.com');
+
+INSERT INTO Orders (order_id, customer_id, product_id, quantity) VALUES
+(5, 4, 3, 2),
+(2, 5, 8, 3),
+(3, 6, 5 ,7);
+
+-- INNER JOIN
+SELECT *
+FROM Orders
+INNER JOIN Customers ON Orders.customer_id = Customers.customer_id;
+
+-- LEFT JOIN
+SELECT *
+FROM Customers
+LEFT JOIN Orders ON Customers.customer_id = Orders.customer_id;
+
+-- RIGHT JOIN
+SELECT *
+FROM Orders
+RIGHT JOIN Customers ON Orders.customer_id = Customers.customer_id;
+
+ -- JOIN
+SELECT *
+FROM Customers
+JOIN Orders ON Customers.customer_id = Orders.customer_id;
+
+-- Self joins
+SELECT *
+FROM Customers c
+JOIN Products p
+ON c.customer_id = p.product_id;
+
+-- Implicit join syntax
+SELECT *
+FROM  Products p, customers 
+WHERE p.product_id = c.customer_id;
+
+-- Self outer joins
+SELECT customer_id,customer_name AS product_id, product_name
+FROM customers c
+JOIN Products p
+ON p.product_id = c.customer_id;
+
+-- NATURAL JOIN
+SELECT product_id, product_name
+FROM Products p
+NATURAL JOIN customers c;
+
+-- CROSS JOIN
+SELECT customer_name AS customer, product_name AS product
+FROM customers c
+CROSS JOIN products p
+ORDER BY customer_name;
+ 
+ -- Unions
+SELECT customer_name
+FROM customers
+UNION
+SELECT customer_id
+FROM customers;
